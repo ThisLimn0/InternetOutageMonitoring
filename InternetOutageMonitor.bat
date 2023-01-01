3f87aaad684a326709771f39d8e88bfd95df26d7
Limn0 <67125885+ThisLimn0@users.noreply.github.com>
2023-01-02 00:36:55 +0100

    Complete refactor to feature 3 different servers

--- InternetOutageMonitor.bat
+++ InternetOutageMonitor.bat
@@ -4,47 +4,64 @@ MODE 120,31
 COLOR 1B
 TITLE Internet Uptime Monitoring - Initialising
 
+::::::::::::::::::::::::::::::::::::::::::::
+SET "Server1=8.8.8.8" ::: Google Public DNS
+SET "Server2=1.0.0.1" ::: Cloudflare DNS
+SET "Server3=9.9.9.9" ::: Quad9 DNS
+::::::::::::::::::::::::::::::::::::::::::::
+
+SET "DefaultTimeout=60"
 SET "Minutes=0"
 SET "InternetConnectedFlag=true"
 
 :MAIN
 SET "LogFile=.\InternetUptime!DATE!.log"
 CALL :GetInternetConnection
-TIMEOUT /T 60 >NUL
-GOTO :MAIN
 
-:GetInternetConnection
-IF /i "!InternetConnectedFlag!"=="true" (
-	SET "Minutes=0"
-)
-PING -n 3 -w 1000 8.8.8.8 | find /i "bytes=" >NUL
-IF !ERRORLEVEL! EQU 0 (
-	IF /i "!InternetConnectedFlag!"=="false" (
-		CALL :GetDate
-		CALL :GetTime
-		CALL :LogProgress
-		ECHO.Event was written into "!LogFile!".
-		ECHO.The connection was re-established.
-	)
-	SET "InternetConnectedFlag=true"
-) ELSE (
-	IF  /i "!InternetConnectedFlag!"=="true" (
-		CALL :GetDate
-		CALL :GetTime
-		SET "InternetOutageStartPoint=!DD!.!MO!.!YYYY! !HH!:!MI!:!SS!"
-	)
-	ECHO.No connection to the Internet. This happened !Minutes! ago. Start of the outage: !InternetOutageStartPoint!
-	SET /A Minutes+=1
-	SET "InternetConnectedFlag=false"
-)
 IF /i "!InternetConnectedFlag!"=="false" (
 	TITLE Internet Uptime Monitoring - Internet not available since !Minutes! minutes. Start:!InternetOutageStartPoint!
+	ECHO.No connection to the Internet. This happened !Minutes! minutes ago. Start of the outage: !InternetOutageStartPoint!
 ) ELSE IF DEFINED InternetOutageStartPoint (
 	TITLE Internet Uptime Monitoring - Internet available. Last outage: !InternetOutageStartPoint!
 ) ELSE (
 	TITLE Internet Uptime Monitoring - Internet available.
 )
 
+TIMEOUT /T !ModifiedTimeout! /NOBREAK >NUL
+GOTO :MAIN
+
+:GetInternetConnection
+IF /i "!InternetConnectedFlag!"=="true" (
+	SET "Minutes=0"
+)
+SET "ModifiedTimeout=!DefaultTimeout!"
+FOR %%X IN (!Server1! !Server2! !Server3!) DO (
+	PING -n 2 -w 1000 %%X | FIND /i "bytes=" >NUL
+	SET /A "ModifiedTimeout-=2"
+	IF !ERRORLEVEL! EQU 0 (
+		IF /i "!InternetConnectedFlag!"=="false" (
+			CALL :GetDate
+			CALL :GetTime
+			CALL :LogProgress
+			ECHO.Event was written into "!LogFile!".
+			ECHO.The connection was re-established.
+			SET /A "ModifiedTimeout-=2"
+		)
+		SET "InternetConnectedFlag=true"
+		GOTO :EOF
+	) ELSE (
+		IF  /i "!InternetConnectedFlag!"=="true" (
+			CALL :GetDate
+			CALL :GetTime
+			SET "InternetOutageStartPoint=!DD!.!MO!.!YYYY! !HH!:!MI!:!SS!"
+			SET "InternetConnectedFlag=false"
+		)
+		SET "InternetConnectedFlag=false"
+	)
+)
+IF /i "!InternetConnectedFlag!"=="false" (
+	SET /A "Minutes+=1"
+)
 EXIT /B
 
 :GetDate
